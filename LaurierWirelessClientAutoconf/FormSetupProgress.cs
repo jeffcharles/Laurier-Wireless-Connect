357294b22c62565db3d747203d27fb24f4a22cce
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-05-20 23:23:59 -0400

    Add GPL v3 license and copyright headers

--- LaurierWirelessClientAutoconf/FormSetupProgress.cs
+++ LaurierWirelessClientAutoconf/FormSetupProgress.cs
@@ -1,4 +1,28 @@
-﻿using System.Windows.Forms;
+﻿#region Copyright
+// <copyright file="LICENSE.txt" company="Open Source at Laurier">
+//
+// Copyright (C) 2010 Open Source at Laurier
+//
+// This file is part of the Laurier Wireless Autoconfiguration Tool.
+//
+// The Laurier Wireless Autoconfiguration Tool is free software: you can
+// redistribute it and/or modify it under the terms of the GNU General 
+// Public License as published by the Free Software Foundation, either 
+// version 3 of the License, or (at your option) any later version.
+//
+// The Laurier Wireless Autoconfiguration Tool is distributed in the 
+// hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+// even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+// PARTICULAR PURPOSE.  See the GNU General Public License for more 
+// details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the Laurier Wireless Autoconfiguration Tool.  If not,
+// see <http://www.gnu.org/licenses/>.
+// </copyright>
+#endregion
+
+using System.Windows.Forms;
 
 namespace OpenSourceAtLaurier.LaurierWirelessClientAutoconf
 {
