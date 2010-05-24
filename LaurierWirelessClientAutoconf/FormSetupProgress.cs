7dfd873e864daa9ec9b8817242ff8e0041b01618
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-05-23 22:15:18 -0400

    Attempt to get commands to execute in background of form

--- LaurierWirelessClientAutoconf/FormSetupProgress.cs
+++ LaurierWirelessClientAutoconf/FormSetupProgress.cs
@@ -31,6 +31,7 @@ namespace OpenSourceAtLaurier.LaurierWirelessClientAutoconf
         public FormSetupProgress()
         {
             InitializeComponent();
+            Program.ExecuteCommands();
         }
     }
 }
